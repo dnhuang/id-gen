99176aecad53b702658e022c72ae8c7955620849
Daniel Huang <dhuang@biointellisense.com>
2025-09-08 23:01:06 -0700

    add config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+Configuration settings for the ID Generator application
+"""
+
+# File upload settings
+MAX_FILE_SIZE_MB = 50
+
+# Processing settings
+MAX_NAMES_COUNT = 10000
+
+# Hash settings
+HASH_ALGORITHM = 'md5'
+
+# Export settings
+CSV_DELIMITER = ','
+
+# Column names
+NAME_COLUMN = 'Name'
+ID_COLUMN = 'ID'
